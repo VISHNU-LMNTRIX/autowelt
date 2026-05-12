24ac206d8794ca01a3c0950115a4679c1fbe9816
Vishnu Prasad A <vishnu.lmntrix@gmail.com>
2026-05-12 19:27:13 +0530

    Updated homepage animations

--- js/blog-preview.js
+++ js/blog-preview.js
@@ -6,40 +6,45 @@ const blogGrid =
 
 if (blogGrid) {
 
-    blogGrid.innerHTML = blogPosts.map(post => `
+    blogGrid.innerHTML = blogPosts.map((post, index) => `
 
-        <article class="blog-card">
+        <article class="reveal-scale delay-${index + 1}">
 
-            <div class="blog-card-image">
+            <div class="blog-card hover-lift">
 
-                <img
-                    src="${post.image}"
-                    alt="${post.title}"
-                >
+                <div class="blog-card-image hover-image">
 
-            </div>
-
-            <div class="blog-card-content">
-
-                <div class="blog-card-meta">
-
-                    <span class="blog-category">
-                        ${post.category}
-                    </span>
-
-                    <span class="blog-date">
-                        ${post.date}
-                    </span>
+                    <img
+                        src="${post.image}"
+                        alt="${post.title}"
+                    >
 
                 </div>
 
-                <h3>${post.title}</h3>
+                <div class="blog-card-content">
 
-                <p>${post.excerpt}</p>
+                    <div class="blog-card-meta">
 
+                        <span class="blog-category">
+                            ${post.category}
+                        </span>
+
+                        <span class="blog-date">
+                            ${post.date}
+                        </span>
+
+                    </div>
+
+                    <h3>${post.title}</h3>
+
+                    <p>${post.excerpt}</p>
+
+                </div>
+                
             </div>
 
         </article>
 
     `).join("");
+
 }
